0cf2f138de22d6692fa3eb9a934c5577e020d58b
Nguyen Phong <ittk54k@gmail.com>
2025-06-06 22:33:42 +0700

    src code gốc

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+  console.log("App component loaded");
+
+  // Return the main content of the app
+  return (
+    <>
+      <h1 className="text-pink-500">MỌI NGƯỜI CODE PHẦN CỦA MÌNH RA FOLDER RIÊNG CÒN TRONG (App.jsx) NÀY LÀ TẬP HỢP LINK ĐỂ CHẠY</h1>
+      <p>Có thể lấy code gợi ý trên Ant Design (npm i antd)</p>
+    </>
+  )
+}
+
+export default App
