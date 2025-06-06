353a9d7d557f75fddd8ce749e0b3dfc62ef2908d
Nguyen Phong <ittk54k@gmail.com>
2025-06-06 22:50:09 +0700

    src code gốc

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -5,8 +5,9 @@ function App() {
   // Return the main content of the app
   return (
     <>
-      <h1 className="text-pink-500">MỌI NGƯỜI CODE PHẦN CỦA MÌNH RA FOLDER RIÊNG CÒN TRONG (App.jsx) NÀY LÀ TẬP HỢP LINK ĐỂ CHẠY</h1>
-      <p>Có thể lấy code gợi ý trên Ant Design (npm i antd)</p>
+      <h1 className="text-pink-500">___MỌI NGƯỜI CODE PHẦN CỦA MÌNH RA FOLDER RIÊNG CÒN TRONG (App.jsx) NÀY LÀ TẬP HỢP LINK ĐỂ CHẠY</h1>
+      <p>___Có thể lấy code gợi ý trên Ant Design (tải về visual hay intel thì code câu này trong terminal npm i antd)</p>
+      <p>___mn code trên vs cho tiện </p>
     </>
   )
 }
