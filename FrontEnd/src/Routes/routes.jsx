138772c07eef33beac0e02e28654f05b1c3d33c6
MinhTrung23042004 <trungnmhe181145@fpt.edu.vn>
2025-06-17 21:35:27 +0700

    manage revenue

--- FrontEnd/src/Routes/routes.jsx
+++ FrontEnd/src/Routes/routes.jsx
@@ -9,6 +9,7 @@ import HomePage from "../Pages/HomePage_phong/homepage.jsx";  // Đổi tên imp
 import Staff from "../Pages/Staff_phong/staff.jsx";  // Đổi tên import cho rõ ràng
 import ManageOrder from "../Pages/ManageOrder_phong/manageorder.jsx";
 import ManageUser from "../Pages/ManageUser_trung/manageuser.jsx";  // Import trang quản lý user mới
+import ManageRevenue from "../Pages/ManageRevenue_trung/managerevenue.jsx";  // Import trang quản lý doanh thu
 
 import Signin from "../Pages/Login_Register_trung/Login.jsx";
 import Register from "../Pages/Login_Register_trung/Register.jsx";
@@ -37,6 +38,7 @@ export default function AppRoutes() {
             <Route path="/staff" element={<Staff />} />
             <Route path="/manageorder" element={<ManageOrder />} />  {/* Quản lý đơn hàng */}
             <Route path="/manageuser" element={<ManageUser />} />  {/* Quản lý user */}
+            <Route path="/managerevenue" element={<ManageRevenue />} />  {/* Quản lý doanh thu */}
             <Route path="/dashboard" element={<Dashboard />} />  {/* Trang Dashboard, có thể là trang chủ hoặc một trang khác */}
             <Route path="/manageorder" element={<ManageOrder />} />  {/* Quản lý đơn hàng */}
             <Route path="/managestaff" element={<StaffManagement />} />
