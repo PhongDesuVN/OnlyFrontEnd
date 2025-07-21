63c1324c7b6c73bf29fedff5b88129765d1e1793
Nguyen Phong <ittk54k@gmail.com>
2025-07-22 05:55:58 +0700

    fix

--- FrontEnd/src/Pages/Staff_phong/DashBoard.jsx
+++ FrontEnd/src/Pages/Staff_phong/DashBoard.jsx
@@ -17,7 +17,6 @@ import {
 } from "lucide-react";
 import { Link } from "react-router-dom";
 import DashBoardApi from "../../utils/DashBoard_phongApi.js";
-
 import Footer from "../../Components/FormLogin_yen/Footer.jsx";
 import {
     BarChart as ReBarChart,
