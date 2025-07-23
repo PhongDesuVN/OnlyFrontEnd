f5c4cdac5ab5e46cb0542476065f31d28773d549
MiTum <huynguyenthai2112@gmail.com>
2025-07-23 23:57:37 +0700

    Sửa lại api tìm kiếm ảnh

--- FrontEnd/src/Pages/Customer_thai/FeedbackModal.jsx
+++ FrontEnd/src/Pages/Customer_thai/FeedbackModal.jsx
@@ -162,11 +162,7 @@ const FeedbackModal = ({ isOpen, onClose, bookingId, storageId, transportId }) =
                                 {storageId && <option value="STORAGE">Kho hàng</option>}
                                 {transportId && <option value="TRANSPORTATION">Vận chuyển</option>}
                             </select>
-                            <div className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                                <svg className="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
-                                </svg>
-                            </div>
+
                         </div>
                     </div>
 
